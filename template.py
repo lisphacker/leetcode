dbda6a9125eb9443df0e7a55adba873d03d0a78e
Gautham Ganapathy <gauthamg@gmail.com>
2020-06-28 01:46:43 +0100

    updted template

--- template.py
+++ template.py
@@ -13,5 +13,11 @@ class Tests(unittest.TestCase):
     def test_1(self):
         self.compare('input', 'output')
 
+    def compare(self, input1,  input2, ref_output):
+        self.assertEqual(ref_output, Solution().apply(input1, input2))
+
+    def test_1(self):
+        self.compare('input1', 'input2', 'output')
+
 if __name__ == '__main__':
     unittest.main()
