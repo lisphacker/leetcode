1700364246a57dc65d1180468479094d4415e4f2
Gautham Ganapathy <gauthamg@gmail.com>
2023-08-24 01:59:48 +0100

    Clean

--- haskell/Problem3.hs
+++ haskell/Problem3.hs
@@ -1,13 +1,6 @@
 module Problem3 where
 
 f :: String -> Int
-f s = f' s 0 0
-  where
-    f' [] s _ = 0
-    f' (x:xs) l m
-      | x `elem` xs = f' xs 0 m
-      | otherwise = f' xs (l + 1) (max (l + 1) m)
-
 i1 = "abcabcbb" :: String
 
 i2 = "bbbbb" :: String
