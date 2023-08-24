2cf6b2fedf77e1ce2d40d568c75fb17a5542e8d8
Gautham Ganapathy <gauthamg@gmail.com>
2023-08-24 11:23:21 +0100

    Problem 3 in Haskell

--- haskell/Problem3.hs
+++ haskell/Problem3.hs
@@ -1,6 +1,21 @@
 module Problem3 where
 
-f :: String -> Int
+import           Data.Set (empty, insert, member, singleton)
+
+f :: String -> String
+f s =
+  let (start, len) = f' s 0 0 0 0 0 empty
+   in take len $ drop start s
+  where
+    f' [] start len _ _ _ _ = (start, len)
+    f' (c:cs) start len s l i set
+      | member c set && l > len = f' cs s l i 1 (i + 1) (singleton c)
+      | member c set && l <= len = f' cs start len i 1 (i + 1) (singleton c)
+      | not (member c set) && l > len =
+        f' cs s l s (l + 1) (i + 1) (insert c set)
+      | not (member c set) && l <= len =
+        f' cs start len s (l + 1) (i + 1) (insert c set)
+
 i1 = "abcabcbb" :: String
 
 i2 = "bbbbb" :: String
--- haskell/README.md
+++ haskell/README.md
@@ -2,4 +2,4 @@
 
 1. [Two Sum](https://leetcode.com/problems/two-sum/) - [Problem1.hs](Problem1.hs)
 2. [Longest Substring Without Repeating Characters
-   ](https://leetcode.com/problems/longest-substring-without-repeating-characters/) - [Problem3.hs](Problem3.hs) (In progress)
+   ](https://leetcode.com/problems/longest-substring-without-repeating-characters/) - [Problem3.hs](Problem3.hs)
